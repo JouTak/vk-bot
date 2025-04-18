6fe598d687e486dfd98985d2f88c8894343921cd
Denis K. Berman <54418849+EnderDissa@users.noreply.github.com>
2025-04-18 18:13:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,31 +3,31 @@ import os
 import getpass
 from pathlib import Path
 
-# required_dirs = [
-#     'xlsx',
-#     'docx'
-# ]
-# required_files = [
-#     'users.yml',
-#     'ignored.txt'
-# ]
+required_dirs = [
+    'source/subscribers'
+]
+required_files = [
+    'source/subscribers/spartakiada24.txt'
+    'source/subscribers/spartakiada.txt'
+]
 
 def create():
-    # for directory in required_dirs:
-    #     if not os.path.exists(directory):
-    #         os.makedirs(directory)
-    #         print(f"Создана папка: {directory}")
-    # for file in required_files:
-    #     path = Path(file)
-    #     if not path.exists():
-    #         path.touch()
-    #         print(f"Создан файл: {file}")
-
+    for directory in required_dirs:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            print(f"Создана папка: {directory}")
+    for file in required_files:
+        path = Path(file)
+        if not path.exists():
+            path.touch()
+            print(f"Создан файл: {file}")
+    """
     if not os.path.exists('token.txt'):
         token=getpass.getpass("введи токен приложения VK:\n")
         with open ('token.txt', 'w') as f:
             f.write(token)
         print(f"токен успешно записан")
+    """
 
 
 if __name__ == "__main__":
