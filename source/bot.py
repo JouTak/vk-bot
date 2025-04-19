45ee16f6f7e4138e810f49acdd26dc3577f54152
Zve1223 <Zve1223@gmail.com>
2025-04-20 02:55:58 +0300

    Sender ultimate update + README sender update + info message update

--- source/bot.py
+++ source/bot.py
@@ -14,10 +14,19 @@ NICKNAME = VK_UID + 1
 GROUP_ID = NICKNAME + 1
 FIO = GROUP_ID + 1
 FIRST_TIME = FIO + 1
+
 WIN_ROUND_1 = FIRST_TIME + 1
 HAS_10_BALLS = WIN_ROUND_1 + 1
 RECORD_ROUND_1 = HAS_10_BALLS + 1
-# isu: (timestamp, vk_uid, link, nick, group, fio, first_time)
+
+WIN_ROUND_2 = RECORD_ROUND_1 + 1
+RECORD_ROUND_2 = WIN_ROUND_2 + 1
+
+FINAL_PLACE = RECORD_ROUND_2 + 1
+RECORD_ROUND_3 = FINAL_PLACE + 1
+
+# UL: isu tsp uid nck grp fio fst wr1 h10 rr1 wr2 rr2 fnl rr3
+# DB: tsp isu uid nck grp fio fst wr1 h10 rr1 wr2 rr2 fnl rr3
 
 groupid = 217494619  # 230160029
 joutek_ip = 'craft.joutak.ru'
@@ -66,18 +75,36 @@ IP: craft.itmo.ru
 Участвуешь ли ты в первом этапе (BlockParty):
 Да.
 
-Переходишь ли в следующий этап:
+Рекорд раундов в блокпати:
 {}
 
 Поставят ли 10 баллов:
 {}
 
-Рекорд раундов в блокпати:
+Переходишь ли в следующий этап:
 {}
-
+{}{}
 Обязательно проверь все данные, только в случае несоответствий или важных вопросов напиши в ответ "АДМИН"
 Читай о нас подробнее на сайте https://joutak.ru/minigames и других разделах
 '''.strip()
+
+second_part = '''
+Рекорд во втором этапе:
+{}
+
+Проходишь ли ты в финал:
+{}
+
+'''
+
+third_part = '''
+Рекорд в третьем этапе:
+{}
+
+Место в финале:
+{}
+
+'''.lstrip()
 
 
 #    'Наш клуб — комьюнити итмошников, которым нравится играть в майнкрафт. ' \
@@ -126,9 +153,16 @@ def ts2str(timestamp: int) -> str:
 
 
 class UserList:
+    s2b = lambda s: s == '1'
+    b2s = lambda b: '1' if b else '0'
+    load2db = (str2ts, int, int, str, str, str, s2b, s2b, s2b, int, s2b, int, int, int)
+    db_types = (str2ts, int, str, str, str, s2b, s2b, s2b, int, s2b, int, int, int)
+    db2save = (ts2str, str, str, str, str, str, b2s, b2s, b2s, str, b2s, str, str, str)
+
     def __init__(self, path: str, vk_helper) -> None:
-        # isu: (timestamp, vk_uid, vk_link, nick, group, fio, first_time)
-        self.db = dict[int: tuple[str, str, str, str, str, str, str, bool, bool, int]]()
+        # UL: isu tsp uid nck grp fio fst wr1 h10 rr1 wr2 rr2 fnl rr3
+        # DB: tsp isu uid nck grp fio fst wr1 h10 rr1 wr2 rr2 fnl rr3
+        self.db = dict[int: tuple[int, int, str, str, str, bool, bool, bool, int, bool, int, int, int]]()
         self.uid_to_isu = dict[int, int]()
         self.path = path
         self.vk_helper = vk_helper
@@ -140,7 +174,7 @@ class UserList:
         if is_file_accessible(self.path) is False:
             return False
         changes = False
-        incorrect_uids = set()
+        incorrect_uids = dict()
         incorrect_isu = 100000
         self.db.clear()
         with open(self.path, 'r', encoding='UTF-8') as file:
@@ -159,39 +193,45 @@ class UserList:
                     warn(f'vk id is NaN (isu = {s[1]}) in {n}-th line in DB:', s[2])
                 # vk_uid не определён, потом определим
                 elif not all(d.isdigit() for d in s[2]):  # vk_uid
-                    incorrect_uids.add(int(s[1]))
+                    incorrect_uids[int(s[1])] = s[2]
+                    s[2] = '0'
                     changes = True
                 if len(s[5].split()) != 3:  # fio
                     warn(f'something wrong with fio (isu = {s[1]}) in {n}-th line in DB:', s[5])
                     # but okay, it's his or her problem
-                if len(s) < 9:
+                if len(s) < 8:
                     s.extend(list('000'))
+                if len(s) < 11:
+                    s.extend(list('00'))
+                if len(s) < 13:
+                    s.extend(list('00'))
                     changes = True
-                # DB   | timestamp isu vk_uid nick    group   fio first_time
-                # Dict | isu: (timestamp, vk_uid, nick, group, fio, first_time)
-                self.db[int(s[1])] = s[0], s[2], s[3], s[4], s[5], s[6], s[7] == '1', s[8] == '1', int(s[9])
+                # UL: isu tsp uid nck grp fio fst wr1 h10 rr1 wr2 rr2 fnl rr3
+                # DB: tsp isu uid nck grp fio fst wr1 h10 rr1 wr2 rr2 fnl rr3
+                s = [f(i) for f, i in zip(self.load2db, s)]
+                self.db[s[1]] = s[0], *s[2:]
         # достаём все vk_uid через vk_link
-        incorrect_uids = sorted(incorrect_uids)
+        incorrect_uids = sorted(incorrect_uids.items())
         for i in range(0, len(incorrect_uids), 25):
             part = incorrect_uids[i:min(i + 25, len(incorrect_uids))]
             links = []
-            for isu in part:
-                start = self.db[isu][VK_UID].rfind('/') + 1
+            for isu, uid in part:
+                start = uid.rfind('/') + 1
                 if start == -1:
-                    start = self.db[isu][VK_UID].find('@') + 1
+                    start = uid.find('@') + 1
                 if start == -1:
                     start = 0
-                links.append(self.db[isu][VK_UID][start:])
+                links.append(uid[start:])
             response = self.vk_helper.links_to_uids(links)
-            for isu, uid in zip(part, response):
-                user = list(self.db[isu])
-                user[VK_UID] = str(uid)
-                self.db[isu] = tuple(user)
+            for pair, uid in zip(part, response):
+                user = list(self.db[pair[0]])
+                user[VK_UID] = int(uid)
+                self.db[pair[0]] = tuple(user)
         # делаем штуку для быстрого доступа к пользователю через uid
         for isu in self.db.keys():
             user = self.db[isu]
-            if user[VK_UID] != '0':
-                self.uid_to_isu[int(user[VK_UID])] = isu
+            if user[VK_UID] != 0:
+                self.uid_to_isu[user[VK_UID]] = isu
         if changes is True:
             return self.save()
         return True
@@ -200,15 +240,16 @@ class UserList:
         if is_file_accessible(self.path) is False:
             return False
         to_save = []
-        for key in self.db.keys():
-            v = self.db[key]
-            to_save.append((str2ts(v[0]), '\t'.join((v[0], str(key), *v[1:-3], '01'[v[-3]], '01'[v[-2]], str(v[-1])))))
+        for isu in self.db.keys():
+            v = self.db[isu]
+            v = [f(i) for f, i in zip(self.db2save, [v[0], isu] + list(v)[1:])]
+            to_save.append((v[0], '\t'.join(v)))
         to_save.sort()
         with open(users_path, 'w', encoding='UTF-8') as file:
             file.write('\n'.join(i[1] for i in to_save))
         return True
 
-    def get(self, isu: int) -> tuple[str, str, str, str, str, str, str] | None:
+    def get(self, isu: int) -> tuple[int, int, str, str, str, bool, bool, bool, int, bool, int, int, int] | None:
         return self.db[isu] if isu in self.db.keys() else None
 
     def keys(self):
@@ -240,13 +281,13 @@ def save_spartakiada_subs(uids: set[int], year: int) -> bool:
 spartakiada24_subs = init_spartakiada_subs(24)
 spartakiada25_subs = init_spartakiada_subs(25)
 
-tokens = [
-    ['|', '&'],
-    ['->', '!>'],
-    ['==', '!='],
-    ['tsp', 'uid', 'nck', 'grp', 'fio', 'fst', 'wr1', 'h10', 'rr1'],
-    ['s24', 's25']
-]
+tokens = (
+    ('|', '&'),
+    ('->', '!>'),
+    ('==', '!=', '>>', '>=', '<<', '<='),
+    ('tsp', 'uid', 'nck', 'grp', 'fio', 'fst', 'wr1', 'h10', 'rr1', 'wr2', 'rr2', 'fnl', 'rr3'),
+    ('s24', 's25', 'adm')
+)
 
 
 def check_condition(cond: str) -> bool:
@@ -271,19 +312,22 @@ def eval_condition(user: tuple, cond: str) -> bool:
         return all(eval_condition(user, i) for i in cond.split('&'))
     if '->' in cond:
         c = cond.split('->')
-        return user[tokens[3].index(c[0])] in [spartakiada24_subs, spartakiada25_subs][tokens[4].index(c[1])]
+        return user[tokens[3].index(c[0])] in [spartakiada24_subs, spartakiada25_subs, admin][tokens[4].index(c[1])]
     if '!>' in cond:
         c = cond.split('!>')
-        return user[tokens[3].index(c[0])] not in [spartakiada24_subs, spartakiada25_subs][tokens[4].index(c[1])]
-    if '==' in cond:
-        c = cond.split('==')
-        print([str(user[tokens[3].index(c[0])]), c[1]])
-        return str(user[tokens[3].index(c[0])]) == c[1]
+        return user[tokens[3].index(c[0])] not in [spartakiada24_subs, spartakiada25_subs, admin][tokens[4].index(c[1])]
+    for token in tokens[2]:
+        if token in cond:
+            c = cond.split(token)
+            index = tokens[3].index(c[0])
+            v = user[index]
+            predicate = (v.__eq__, v.__ne__, v.__gt__, v.__ge__, v.__lt__, v.__le__)
+            return predicate[index](UserList.db_types[index](c[1]))
     if '!=' in cond:
         c = cond.split('!=')
         return str(user[tokens[3].index(c[0])]) != c[1]
     return False
-        
+
 
 def sender(self, condition: str, msg: str) -> list[dict]:
     if check_condition(condition) is False:
@@ -297,7 +341,6 @@ def sender(self, condition: str, msg: str) -> list[dict]:
             continue
         if eval_condition(user, condition) is True:
             result.append({'peer_id': uid, 'message': msg})
-    print(*result, len(result), sep='\n')
     return result
 
 
@@ -317,7 +360,6 @@ def process_message_event(self, event, vk_helper) -> list[dict] | None:
 
 # Чёт без кнопки
 def process_message_new(self, event, vk_helper, ignored) -> list[dict] | None:
-    tts = ''
     users = self.users
     uid = event.message.from_id
 
@@ -396,9 +438,11 @@ def process_message_new(self, event, vk_helper, ignored) -> list[dict] | None:
             user = users.get(isu)
             tts = welcome_message.format(
                 isu, user[NICKNAME],
-                ['Нет', 'Да'][user[WIN_ROUND_1]], ['Нет', 'Да'][user[HAS_10_BALLS]], user[RECORD_ROUND_1])
+                ['Нет', 'Да'][user[WIN_ROUND_1]], ['Нет', 'Да'][user[HAS_10_BALLS]], user[RECORD_ROUND_1],
+                second_part.format(user[RECORD_ROUND_2], ['Нет', 'Да'][user[WIN_ROUND_2]]) if user[WIN_ROUND_1] else '',
+                third_part.format(user[RECORD_ROUND_3], user[FINAL_PLACE]) if user[WIN_ROUND_2] else '')
         else:
-            tts = 'Кажется, у нас нет твоих данных. Позови админа'
+            tts = 'Кажется, у нас нет твоих данных. Позови админа, если это не должно быть так'
     return [{
         'peer_id': uid,
         'message': tts
